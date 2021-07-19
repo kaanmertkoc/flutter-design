6d591cc0273ab690e581ed785d6fbb1868946db4
kaanmertkoc <46743238+kaanmertkoc@users.noreply.github.com>
2021-07-19 14:11:56 +0300

    Sliding up panel made. Continue watching cards implement.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,6 +3,7 @@ import 'package:code_design/components/lists/recent_course_list.dart';
 import 'package:code_design/screens/sidebar_screen.dart';
 import 'package:flutter/material.dart';
 import '../../constants.dart';
+import 'continue_watching_screen.dart';
 import 'home_screen_navbar.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -100,6 +101,7 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
                 ],
               ),
             ),
+            ContinueWatchingWidget(),
             IgnorePointer(
               ignoring: sidebarHidden,
               child: Stack(
